9f0386924a01d4ed0544a8ee414f86aca11b7128
James Vaughan <James.Vaughan@utoronto.ca>
2020-02-14 11:08:48 -0500

    Switch Location to Rectangle. (#85)
    * Switched from using a point for location to using a Rectangle.
    
    * Added methods in the ModelSystemEditingSession to set a nodes location.

---
+++ src/XTMF2/ModelSystemConstruct/Rectangle.cs
@@ -0,0 +1,76 @@
+﻿/*
+    Copyright 2017 University of Toronto
+
+    This file is part of XTMF2.
+
+    XTMF2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XTMF2
+{
+    public readonly struct Rectangle
+    {
+        public Rectangle(float x, float y, float width = 80, float height = 50)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public float X { get; }
+        public float Y { get; }
+
+        public float Width { get; }
+
+        public float Height { get; }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is Rectangle other)
+            {
+                return other.X == X & other.Y == Y & other.Width == Width & other.Height == Height;
+            }
+            return false;
+        }
+
+        public bool Equals(Rectangle other)
+        {
+            return other.X == X & other.Y == Y & other.Width == Width & other.Height == Height;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Width, Height);
+        }
+
+        public static bool operator ==(Rectangle left, Rectangle right)
+        {
+            return left.X == right.X & left.Y == right.Y & left.Width == right.Width & left.Height == right.Height;
+        }
+
+        public static bool operator !=(Rectangle left, Rectangle right)
+        {
+            return left.X != right.X | left.Y != right.Y | left.Width != right.Width | left.Height != right.Height;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{X},{Y},{Width},{Height}}}";
+        }
+    }
+}
