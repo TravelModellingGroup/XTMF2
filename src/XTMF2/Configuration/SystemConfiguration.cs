090e5496bbf9775efe192872456c2a3b997b7c7c
James Vaughan <James.Vaughan@utoronto.ca>
2026-03-10 14:49:12 -0400

    Automatically load modules in the subdirectory Modules (#150)

--- src/XTMF2/Configuration/SystemConfiguration.cs
+++ src/XTMF2/Configuration/SystemConfiguration.cs
@@ -1,5 +1,5 @@
 ﻿/*
-    Copyright 2017 University of Toronto
+    Copyright 2017-2026 University of Toronto
 
     This file is part of XTMF2.
 
@@ -17,123 +17,129 @@
     along with XTMF2.  If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using XTMF2.Editing;
 using XTMF2.Repository;
 using System.Runtime.Loader;
 using System.Linq;
 
-namespace XTMF2.Configuration
+namespace XTMF2.Configuration;
+
+/// <summary>
+/// This class contains the configuration data
+/// for the full XTMF runtime. It does not contain any
+/// user level configuration.
+/// </summary>
+public class SystemConfiguration
 {
     /// <summary>
-    /// This class contains the configuration data
-    /// for the full XTMF runtime. It does not contain any
-    /// user level configuration.
+    /// The repository of modules available to this XTMF runtime
     /// </summary>
-    public class SystemConfiguration
+    public ModuleRepository Modules { get; private set; }
+
+    /// <summary>
+    /// The repository of all different available types available to this XTMF runtime
+    /// </summary>
+    public TypeRepository Types { get; private set; }
+
+    /// <summary>
+    /// The path to the default user directory
+    /// </summary>
+    public string DefaultUserDirectory { get; private set; }
+
+    /// <summary>
+    /// Create a new system configuration for the given XTMF Runtime.
+    /// </summary>
+    /// <param name="runtime">The runtime to bind to.</param>
+    /// <param name="fullPath">Optional, the path to the system configuration.</param>
+    public SystemConfiguration(XTMFRuntime runtime, string? fullPath = null)
     {
-        /// <summary>
-        /// The repository of modules available to this XTMF runtime
-        /// </summary>
-        public ModuleRepository Modules { get; private set; }
-
-        /// <summary>
-        /// The repository of all different available types available to this XTMF runtime
-        /// </summary>
-        public TypeRepository Types { get; private set; }
-
-        /// <summary>
-        /// The path to the default user directory
-        /// </summary>
-        public string DefaultUserDirectory { get; private set; }
-
-        /// <summary>
-        /// Create a new system configuration for the given XTMF Runtime.
-        /// </summary>
-        /// <param name="runtime">The runtime to bind to.</param>
-        /// <param name="fullPath">Optional, the path to the system configuration.</param>
-        public SystemConfiguration(XTMFRuntime runtime, string? fullPath = null)
+        CreateDirectory(DefaultUserDirectory =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "XTMF2", "Users"));
+        Modules = new ModuleRepository();
+        Types = new TypeRepository();
+        // Load the entry assembly for types
+        LoadAssembly(Assembly.GetEntryAssembly()!);
+        // Load the baked in XTMF2 modules
+        LoadAssembly(typeof(SystemConfiguration).GetTypeInfo().Assembly);
+        // Load the assemblies in the assembly's "Modules" directory.
+        var libraryLocation = Path.GetDirectoryName(typeof(SystemConfiguration).GetTypeInfo().Assembly.Location);
+        if (libraryLocation is not null)
         {
-            CreateDirectory(DefaultUserDirectory =
-                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "XTMF2", "Users"));
-            Modules = new ModuleRepository();
-            Types = new TypeRepository();
-            // Load the entry assembly for types
-            LoadAssembly(Assembly.GetEntryAssembly()!);
-            // Load the baked in XTMF2 modules
-            LoadAssembly(typeof(SystemConfiguration).GetTypeInfo().Assembly);
-        }
-
-        private void CreateDirectory(string directoryName)
-        {
-            DirectoryInfo dir = new DirectoryInfo(directoryName);
-            if (!dir.Exists)
+            var modulesDir = Path.Combine(libraryLocation, "Modules");
+            if (Directory.Exists(modulesDir))
             {
-                dir.Create();
+                LoadAssemblies(modulesDir, Path.Combine(modulesDir, "exclude.txt"));
             }
-        }
-
-        /// <summary>
-        /// Load an assembly from the given path into the system's configuration.
-        /// </summary>
-        /// <param name="path">The path to the assembly to load.</param>
-        public void LoadAssembly(string path)
-        {
-            var fullPath = Path.GetFullPath(path);
-            LoadAssembly(AssemblyLoadContext.Default.LoadFromAssemblyPath(fullPath));
-        }
-
-        /// <summary>
-        /// Load the assemblies from the given directory into the system's configuration.
-        /// </summary>
-        /// <param name="path">The directory to load the assemblies from.</param>
-        /// <param name="exclusionFile">A text file containing the assemblies to not load.</param>
-        public void LoadAssemblies(string path, string? exclusionFile = null)
-        {
-            var exludedDlls = exclusionFile is null ? new List<string>() : File.ReadLines(exclusionFile).ToList();
-            LoadAssemblies(path, exludedDlls);
-        }
-
-        /// <summary>
-        /// Load the assemblies from the given directory into the system's configuration.
-        /// </summary>
-        /// <param name="path">The directory to load the assemblies from.</param>
-        /// <param name="toExclude">A list of DLL files to exclude from loading.</param>
-        public void LoadAssemblies(string path, List<string> toExclude)
-        {
-            var dirInfo = new DirectoryInfo(path);
-            foreach (var dllFile in dirInfo.EnumerateFiles("*.dll"))
-            {
-                if (!toExclude.Contains(dllFile.Name))
-                {
-                    LoadAssembly(AssemblyLoadContext.Default.LoadFromAssemblyPath(dllFile.FullName));
-                }
-            }
-        }
-
-
-        /// <summary>
-        /// Load the types of the given assembly
-        /// </summary>
-        /// <param name="assembly">The assembly to load</param>
-        public void LoadAssembly(Assembly assembly)
-        {
-            if (assembly == null)
-            {
-                throw new ArgumentNullException(nameof(assembly));
-            }
-            Parallel.ForEach(assembly.ExportedTypes, (Type t) =>
-            {
-                string? error = null;
-                Modules.AddIfModuleType(t);
-                Types.Add(t, ref error);
-            });
         }
     }
+
+    private void CreateDirectory(string directoryName)
+    {
+        DirectoryInfo dir = new DirectoryInfo(directoryName);
+        if (!dir.Exists)
+        {
+            dir.Create();
+        }
+    }
+
+    /// <summary>
+    /// Load an assembly from the given path into the system's configuration.
+    /// </summary>
+    /// <param name="path">The path to the assembly to load.</param>
+    public void LoadAssembly(string path)
+    {
+        var fullPath = Path.GetFullPath(path);
+        LoadAssembly(AssemblyLoadContext.Default.LoadFromAssemblyPath(fullPath));
+    }
+
+    /// <summary>
+    /// Load the assemblies from the given directory into the system's configuration.
+    /// </summary>
+    /// <param name="path">The directory to load the assemblies from.</param>
+    /// <param name="exclusionFile">A text file containing the assemblies to not load.</param>
+    public void LoadAssemblies(string path, string? exclusionFile = null)
+    {
+        var exludedDlls = exclusionFile is null || !File.Exists(exclusionFile) ? [] : File.ReadLines(exclusionFile).ToList();
+        LoadAssemblies(path, exludedDlls);
+    }
+
+    /// <summary>
+    /// Load the assemblies from the given directory into the system's configuration.
+    /// </summary>
+    /// <param name="path">The directory to load the assemblies from.</param>
+    /// <param name="toExclude">A list of DLL files to exclude from loading.</param>
+    public void LoadAssemblies(string path, List<string> toExclude)
+    {
+        var dirInfo = new DirectoryInfo(path);
+        foreach (var dllFile in dirInfo.EnumerateFiles("*.dll"))
+        {
+            if (!toExclude.Contains(dllFile.Name))
+            {
+                LoadAssembly(AssemblyLoadContext.Default.LoadFromAssemblyPath(dllFile.FullName));
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Load the types of the given assembly
+    /// </summary>
+    /// <param name="assembly">The assembly to load</param>
+    public void LoadAssembly(Assembly assembly)
+    {
+        if (assembly == null)
+        {
+            throw new ArgumentNullException(nameof(assembly));
+        }
+        Parallel.ForEach(assembly.ExportedTypes, (Type t) =>
+        {
+            string? error = null;
+            Modules.AddIfModuleType(t);
+            Types.Add(t, ref error);
+        });
+    }
 }
+
